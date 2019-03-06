47c9c0dcce665585a15794184fe414ba4575fd84
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-06 20:56:04 +1100

    Entire app deleted and started from scratch without create react app. Using Webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Base react app
+      Im going to add all sorts of stuff tomorrow!!!!!
+      Yay.
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
