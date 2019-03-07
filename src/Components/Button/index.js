1a59feb75f185e6f85cbb5fb5e25bc3165193a33
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-07 15:46:27 +1100

    Button fixed - untypescripted

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export class Button extends Component {
-  public render() {
+  render() {
     return (
       <button className="btn waves-effect waves-light" type="submit" name="action">
         {this.props.label}
