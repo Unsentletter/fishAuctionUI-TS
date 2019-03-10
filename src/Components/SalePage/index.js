9dbc26000196f22a2c21a67a3ace0122da8b4800
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-10 17:46:37 +1100

    Sale page created

---
+++ src/Components/SalePage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {ItemList} from "../ItemList";
+
+export class SalePage extends Component {
+  render() {
+    return(
+      <div>
+        <ItemList/>
+      </div>
+    )
+  }
+}
