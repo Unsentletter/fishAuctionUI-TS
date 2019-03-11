5b2f7f274f5b8aa212100ebb1946344f62b40855
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-11 22:06:23 +1100

    ItemCard styled and linked to ItemPage

--- src/Components/ItemCard/index.js
+++ src/Components/ItemCard/index.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './index.scss';
 
-export class ItemCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    console.log("Card PROPS", this.props);
-    return (
+export const ItemCard = props => {
+  return (
+    <Link to={'/item_page'}>
       <div className='card horizontal'>
         <div className="card-image">
           <img src="../../../public/guppy.jpg" alt="This is a photo"/>
@@ -17,14 +13,18 @@ export class ItemCard extends Component {
         <div className='card-stacked'>
           <div className='card-text'>
             <div className='card-top-line'>
-              <div className='name'>{this.props.name}</div>
-              <div className='price'>{this.props.price}</div>
+              <div className='name'>{props.name}</div>
+              <div className='price'>{props.price}</div>
             </div>
-            <div>Location</div>
+            <div className='name-line'>
+              <div>Poster name, </div>
+              <div>Location</div>
+            </div>
             <div>Time posted</div>
+            <div className='description'>Item description - This fish is very fishy. It likes to swim, eat food, and make babies. It is a fan of the oxford comma and is willing to egg your house if you think any differently about it. It will also shit on your windscreen if you eat his turkey sandwiches</div>
           </div>
         </div>
       </div>
-    )
-  }
+    </Link>
+  )
 }
