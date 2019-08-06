5932d2edd9f38bfd89bd32389ff8d4c72e75c891
Tristan Southwell <tristan.southwell@hotmail.com>
2019-08-06 21:06:24 +1000

    Fixed weird babel error that randomly appeared

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,29 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: ["@babel/polyfill", "./src/index.js"],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
+      template: "./src/index.html",
+      filename: "./index.html"
     })
   ]
 };
